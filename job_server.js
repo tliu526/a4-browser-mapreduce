1d77841f412e6098569c6b8e47325551f65cf16f
tliu526 <tl4@williams.edu>
2016-04-29 02:26:51 -0400

    Can push computation to workers now

--- job_server.js
+++ job_server.js
@@ -5,6 +5,7 @@
 
 //"Include" statements
 var http = require('http');
+var url = require('url');
 var structs = require('./structs');
 
 /**** WEB SERVER FUNCTIONS AND VARS ****/
@@ -15,9 +16,35 @@ const PORT = 8889;
  * TODO actually handle requests
  */
 function request_handler(request, response){
-    response.end('Hello world! Path hit: ' + request.url);
-}
+//    console.log(url.parse(request.url).pathname);
+//    response.end('Hello world! Path hit: ' + request.url);
+   
+    if(request.method == 'GET'){
+        var html = create_task_html(add.toString(), "add", "1,2,3");
+        //console.log(html);
+        response.writeHead(200, {
+            'Content-Type' : 'text/html',
+            'Content-Length' : html.length,
+            'Expires' : new Date().toUTCString(),
+            'Access-Control-Allow-Origin' : '*'
+        });
+        response.end(html);
+    }
+    else if(request.method == 'POST'){
+        console.log('got a post request!');
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
+            console.log("Body: " + body);
+            //Parse the results here!!
+        });
 
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end('POST received');
+    }
+}
 
 /**** JOB MANAGEMENT FUNCTIONS AND VARS ****/
 
@@ -47,6 +74,67 @@ function create_map_tasks(map, data, job_id){
  */
 function create_reduce_tasks(){}
 
+/**
+ * Creates the worker html
+ * TODO think about parameters more, this is just a working prototype
+ * task is currently the javascript code 
+ * task_name is currently the function name implemented in task
+ */
+function create_task_html(task, task_name, data){
+    var html = '<!DOCTYPE html>';
+    html += '<html> <head> <script type=\"text/javascript\">\n';
+    html += task;
+    html += createCORSRequest.toString();
+    html += "</script> </head>";
+    html += "<body onload=" + "\"" + task_name + "();\"" + ">\n";
+    html += data;
+    html += "</body>";
+    html += "</html>";
+
+    return html;
+}
+/** a test function */
+function add(){
+    var vals = document.body.innerHTML.split(",");
+    //maps the strings in val to numbers
+    for(var i = 0; i < vals.length; i++){
+        vals[i] = parseInt(vals[i],10);
+    }
+    var total = vals.reduce(function(a,b){
+        return a + b;
+    });
+    //alert(total);
+    //var xhttp = new XMLHTTPRequest();
+    //xhttp.open("POST", "http://localhost:8889", true);
+    //xhttp.send("result="+total);
+    var request = createCORSRequest("post", "http://localhost:8889");
+    if(request){
+        request.send(total);
+    }
+/*
+    var form = createElement("form", {
+        action : "http://localhost:8889",
+        method : "POST"
+    });
+    form.setAttribute("action", "http://localhost:8889");
+    form.setAttribute("method", "POST");
+    document.body.appendChild(form);
+    form.submit();
+*/
+}
+
+function createCORSRequest(method, url){
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr){
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
+}
 
 /**** MAIN ****/
 function main(){

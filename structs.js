9bbd7a1d19edfc9008dc9c27ee093cd7b0d6a9b6
tliu526 <tl4@williams.edu>
2016-04-30 21:28:32 -0400

    Implemented a simple queue

--- structs.js
+++ structs.js
@@ -3,6 +3,7 @@
  * (c) 2016 Tony Liu, Michael Shaw.
  */
 module.exports = {
+    /*
     enq: function(queue, val){
         queue.push(val);
     },
@@ -10,6 +11,25 @@ module.exports = {
     deq: function(queue){
         var val = queue.shift();
         return val;
+    },
+    */
+
+    Queue: function(data){
+        data = typeof data !== 'undefined' ? data: [];
+        this.data = data;
+
+        this.enq = function(val){
+            data.push(val);
+        }
+
+        this.deq = function(){
+            var val = data.shift();
+            return val;
+        }
+
+        this.is_empty = function(){
+            return data.length <= 0;
+        }
     },
 
     //A task to be completed by a volunteer node, can either be a map or reduce.

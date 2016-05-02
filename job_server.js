b7270ac899c3db2ab5934b95295d00408d875620
tliu526 <tl4@williams.edu>
2016-05-01 22:06:50 -0400

    merge attempt

--- job_server.js
+++ job_server.js
@@ -21,6 +21,20 @@ function request_handler(request, response){
 //    response.end('Hello world! Path hit: ' + request.url);
    
     if(request.method == 'GET'){
+        var form = create_SAML_form();
+	var script = wrap_form();
+	var html = '<html>\n';
+	html += form + '\n';
+	html += '</html>';
+
+	response.writeHead(200, {
+	    'Content-Type' : 'text/html',
+	    'Content-Length' : html.length,
+	    'Access-Control-Allow-Origin' : '*'
+	});
+	response.end(html);
+
+	/*
         var nums = ''
         for(var i = 0; i < 5; i++){
             var n = Math.floor(Math.random() * (10));
@@ -40,6 +54,8 @@ function request_handler(request, response){
             'Access-Control-Allow-Origin' : '*'
         });
         response.end(html);
+	
+	*/
     }
 
     else if(request.method == 'POST'){
@@ -64,6 +80,31 @@ var map_tasks = []; //Queue of structs.Tasks
 var reduce_tasks = []; //Queue of structs.Tasks
 
 var job_id = 0; //Global job ID 
+
+
+/**
+ * Creates an HTML form to send to the user with a SAML Authentication Request
+ * TODO Put real values for SAMLRequest and RelayState
+ */
+function create_SAML_form() {
+    var form = '<form method=\"POST\" action=\"http://localhost:8890\" id=\"form\">\n';
+    form += '<input type=\"hidden\" name=\"SAMLRequest\" value = \"request\" />\n';
+    form += '<input type=\"hidden\" name=\"RelayState\" value=\"state\" />\n';
+    form += '<input type=\"submit\" value=\"Click here to be authenticated\" />\n';
+    form += '</form>\n';
+    return form;
+}
+
+/**
+ * Wraps an HTML form (named form) in a script that automatically submits it
+ */
+function wrap_form() {
+    var script = '<script type=\"text/javascript\">\n';
+    script += 'function submitForm() {\n';
+    script += 'document.getElementById(\"form\").submit();\n';
+    script += '</script>';
+    return script;
+}
 
 
 /**
